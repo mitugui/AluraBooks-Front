87effbd26235cb71fd53395663809ad23e1363f4
Cristian Oliveira Mitugui <cristian.mitugui04@gmail.com>
2024-04-21 19:29:21 -0300

    Estilizando a página de favoritos

--- alura-books/src/routes/Favorites.js
+++ alura-books/src/routes/Favorites.js
@@ -1,11 +1,46 @@
 import styled from 'styled-components'
 import { useEffect, useState } from 'react'
 import { getFavorites } from '../services/favorite'
+import bookImg from '../img/book.png'
 
 const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
   background-image: linear-gradient(90deg, #002F52 35%, #326589);
+`
+
+const ResultContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+`
+
+const Result = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px 0;
+    cursor: pointer;
+    text-align: center;
+    padding: 0 100px;
+    p {
+        width: 200px;
+        color: #FFF;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
+`
+
+const Title = styled.h2`
+    color: #FFF;
+    font-size: 36px;
+    text-align: center;
+    width: 100%;
+    padding-top: 35px
 `
 
 function Favorites() {
@@ -22,9 +57,19 @@ function Favorites() {
 
   return (
     <AppContainer>
-      {favorites.map(favorite => (
-        <p>{favorite.name}</p>
-      ))}
+      <div>
+        <Title>Aqui estão seus livros favoritos:</Title>
+        <ResultContainer>
+          {
+            favorites.length !== 0 ? favorites.map(favorite => (
+              <Result>
+                <p>{favorite.name}</p>
+                <img src={bookImg} />
+              </Result>
+            )) : null
+          }
+        </ResultContainer>
+      </div>
     </AppContainer>
   )
 }
